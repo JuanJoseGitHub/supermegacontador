fd4b233c1d15b40bf7f698042287d2406d454978
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-23 16:22:54 +0100

    Numero de veces botón

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+  
+  let [valor, setValor] = useState(0);
+  let [sumas,setSumas] = useState (0);
+  let [restas,setRestas] = useState (0);
+
+  function sumaUn() {
+    setValor(++valor);
+    setSumas(++sumas);
+  }
+  function restaUn() {
+    setValor(--valor);
+    setRestas(++restas);
+  }
+  function reset() {
+    setValor(0);
+  }
+
+  return (
+    <>
+      <h1>{valor}</h1>
+      <button onClick={sumaUn}> + {sumas}</button>
+      <button onClick={restaUn}> - {restas}</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  );
+}
+
+export default App;
